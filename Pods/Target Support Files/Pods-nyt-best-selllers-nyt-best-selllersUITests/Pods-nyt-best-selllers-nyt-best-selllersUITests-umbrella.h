444e70cc583207bc7c28a14d7ff1c43caa47e3a3
Farhana Mustafa <farhana_mustafa@intuit.com>
2021-01-30 12:30:34 -0500

    adding pods

---
+++ Pods/Target Support Files/Pods-nyt-best-selllers-nyt-best-selllersUITests/Pods-nyt-best-selllers-nyt-best-selllersUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_nyt_best_selllers_nyt_best_selllersUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_nyt_best_selllers_nyt_best_selllersUITestsVersionString[];
+
---
+++ Pods/Target Support Files/Pods-nyt-best-selllersTests/Pods-nyt-best-selllersTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nyt_best_selllersTests : NSObject
+@end
+@implementation PodsDummy_Pods_nyt_best_selllersTests
+@end
