8ec129007964445ce3820ff56d8f243b88aba320
Farhana Mustafa <farhana_mustafa@intuit.com>
2021-01-31 15:12:03 -0500

    removing service file and adding kingfisher code

--- nyt-best-selllers/View/ViewController.swift
+++ nyt-best-selllers/View/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
                                      bottom: 10.0,
                                      right: 10.0)
     var viewModel: BookViewModel!
-    var service = BookService()
     let apiKey = Config.apiKey
     
     var books = [Book]()
@@ -34,28 +33,15 @@ class ViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-//        flowLayout.scrollDirection = .horizontal
-//        flowLayout.minimumLineSpacing = 0
-//        flowLayout.minimumInteritemSpacing = 0
-//        flowLayout.sectionInset = sectionInsets
-        
-        let itemSize = UIScreen.main.bounds.width/2 - 3
-
-        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        flowLayout.itemSize = CGSize(width: itemSize, height: itemSize)
-
-        flowLayout.minimumInteritemSpacing = 3
-        flowLayout.minimumLineSpacing = 3
     }
     
     func loadBookDataFromService() {
+        // TODO: add query params
         let baseUrl = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key="
         let urlString = baseUrl + apiKey
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
-//                service.parse(json: data)
                 parse(json: data)
             }
         }
@@ -75,9 +61,7 @@ class ViewController: UIViewController {
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookCell", for: indexPath) as! BookCell
-//        cell.bookCover.load(urlString: viewModel.books[0].book_image)
-        print(books)
-        cell.bookCover.load(urlString: books[indexPath.row].book_image)
+        cell.bookCover.loadImageKf(urlString: books[indexPath.row].book_image, imageView: cell.bookCover)
         return cell
     }
 
@@ -117,16 +101,19 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
 }
 
 extension UIImageView {
-    func load(urlString: String) {
-        DispatchQueue.global().async { [weak self] in
-            if let url = URL(string: urlString) {
-                if let data = try? Data(contentsOf: url) {
-                    if let image = UIImage(data: data) {
-                        DispatchQueue.main.async {
-                            self?.image = image
-                        }
-                    }
-                }
+    func loadImageKf(urlString: String, imageView: UIImageView) {
+        let url = URL(string: urlString)
+
+        // set the image view via kingfisher
+        imageView.kf.setImage(with: url, options: [
+            .transition(.fade(1)),
+            .cacheOriginalImage
+        ]) { (result) in
+            switch result {
+            case .success(let image):
+                imageView.image = image.image
+            case .failure(_):
+                print("Failed to load provider image via Kingfisher.")
             }
         }
     }
