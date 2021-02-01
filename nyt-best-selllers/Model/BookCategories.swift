f3224502b87364ef807799cb538aeb397ad7b8ba
Farhana Mustafa <farhana_mustafa@intuit.com>
2021-02-01 10:26:53 -0500

    adding book categories

---
+++ nyt-best-selllers/Model/BookCategories.swift
@@ -0,0 +1,18 @@
+//
+//  BookCategories.swift
+//  nyt-best-sellers
+//
+//  Created by fmustafa on 1/31/21.
+//
+
+import Foundation
+
+enum BookCategory {
+    case fiction
+    case combinedPrintAndEbookFiction
+    case hardCoverFiction
+    case paperbackTradeFiction
+    case nonfiction
+    case combinedPrintAndEbookNonFiction
+    case childrens
+}
